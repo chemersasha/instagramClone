0d13d9708be8eda894c7e9f6d3bd9d472d550230
chemer <chemer@ua.fm>
2017-05-15 18:01:27 +0300

    Added entity to get data from Instagram json response

---
+++ ACInsagramFeed/ACInstagramJsonParser.h
@@ -0,0 +1,13 @@
+//
+//  ACInstagramJsonParser.h
+//  ACInsagramFeed
+//
+//  Created by Chemersky on 5/15/17.
+//  Copyright © 2017 Chemer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ACInstagramJsonParser : NSObject
+
+@end
---
+++ ACInsagramFeed/ACInstagramJsonParser.m
@@ -0,0 +1,13 @@
+//
+//  ACInstagramJsonParser.m
+//  ACInsagramFeed
+//
+//  Created by Chemersky on 5/15/17.
+//  Copyright © 2017 Chemer. All rights reserved.
+//
+
+#import "ACInstagramJsonParser.h"
+
+@implementation ACInstagramJsonParser
+
+@end
